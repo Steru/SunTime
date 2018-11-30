43a0351704aaef010eae0d62e91f9e96635843ba
Steru <s.stergiadis@gmail.com>
2018-11-30 23:11:27 +0100

    Fix wrong name in view xml, go back to gradle v4.6
    Gradle 5.0 was taking too long to build, so 4.6 was used back again.
    CI should hopefully work.

--- app/src/main/java/com/just/suntime/models/SunInfo.kt
+++ app/src/main/java/com/just/suntime/models/SunInfo.kt
@@ -2,4 +2,4 @@ package com.just.suntime.models
 
 import java.util.*
 
-data class SunInfo(val sunRise: Date, val sunSet: Date, val dayLengthInSecs: Int)
+data class SunInfo(val sunRise: Date, val sunSet: Date, val dayLengthSeconds: Int)
